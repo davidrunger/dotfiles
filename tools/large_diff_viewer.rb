ccd44a3500ec1bceddac0b73f2f5c7f164026612
David Runger <david.runger@commonlit.org>
2023-10-29 18:29:29 -0500

    [large_diff_viewer] Clear scratch and use new hard-clear executable

--- tools/large_diff_viewer.rb
+++ tools/large_diff_viewer.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
+# Goes through changed files (relative to the main branch) one at a time.
+
+def clear_scratch
+  scratch!(nil, silent: true)
+end
+
 last_viewed_file = Runger.config.scratch
 
 changed_files = `gf`.rstrip.split("\n")
+
+if !last_viewed_file.in?(changed_files)
+  clear_scratch
+  last_viewed_file = nil
+end
 
 changed_files.
   each.with_index(1) do |file, position|
@@ -8,9 +21,11 @@ changed_files.
       next
     end
 
-    system("clear && printf '\e[3J'", exception: true)
+    system("hard-clear", exception: true)
     puts("#{position} / #{changed_files.size}")
     system({ "DELTA_PAGER" => "cat" }, "gdom #{file}", exception: true)
     gets
     scratch!(file, silent: true)
   end
+
+clear_scratch

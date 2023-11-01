1646a633349b51280b3da4a3cf35dc94e0d22f14
David Runger <davidjrunger@gmail.com>
2023-10-31 23:43:01 -0500

    Move commonlit files into specific directory

--- install.sh
+++ install.sh
@@ -5,15 +5,20 @@ mkdir -p ~/.config/
 ln -sf ~/code/dotfiles/aprc.rb ~/.config/aprc
 ln -sf ~/code/dotfiles/gemrc.yml ~/.gemrc
 ln -sf ~/code/dotfiles/global_gitignore ~/.gitignore
-ln -sf ~/code/dotfiles/initializers/z.rb ~/code/commonlit/config/initializers/z.rb
 ln -sf ~/code/dotfiles/irbrc.rb ~/.irbrc.rb
 ln -sf ~/code/dotfiles/pryrc.rb ~/.pryrc
-ln -sf ~/code/dotfiles/qr.rb ~/code/commonlit/personal/qr.rb
 ln -sf ~/code/dotfiles/rspec ~/.rspec
 ln -sf ~/code/dotfiles/rubocop.yml ~/.rubocop.yml
-ln -sf ~/code/dotfiles/workers/load_runner.rb ~/code/commonlit/app/workers/load_runner.rb
 ln -sf ~/code/dotfiles/zsh/themes/bolso.zsh-theme ~/.oh-my-zsh/custom/themes/bolso.zsh-theme
 ln -sf ~/code/dotfiles/zshrc.sh ~/.zshrc
+
+if [ -d "~/code/commonlit/" ]; then
+  ln -sf ~/code/dotfiles/commonlit/initializers/z.rb ~/code/commonlit/config/initializers/z.rb
+  ln -sf ~/code/dotfiles/commonlit/personal/qr.rb ~/code/commonlit/personal/qr.rb
+  ln -sf \
+    ~/code/dotfiles/commonlit/workers/load_runner.rb \
+    ~/code/commonlit/app/workers/load_runner.rb
+fi
 
 touch ~/.hushlogin
 touch ~/.pry_history

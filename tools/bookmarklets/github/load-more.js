0d587d4c2bd5c1cb28ad249476baa311125db5a5
David Runger <david.runger@commonlit.org>
2024-01-10 08:09:06 -0600

    [bookmarklets/github/load-more] Add "load more" bookmarklet
    (This is not actually a bookmarklet; this is source code for a
    bookmarklet. Paste this into e.g.
    https://caiorss.github.io/bookmarklet-maker/ to convert this to
    bookmarklet code.)

---
+++ tools/bookmarklets/github/load-more.js
@@ -0,0 +1,20 @@
+const targetSelector = '.ajax-pagination-btn';
+const targetText = 'Load more';
+
+function setButtonClickTimeouts() {
+  const loadMoreButtons = [...document.querySelectorAll(targetSelector)].filter(
+    (el) => el.innerText.includes(targetText)
+  );
+
+  [...loadMoreButtons, 'setIntervalPlaceholder'].forEach((el, index) => {
+    setTimeout(() => {
+      if (el.click) {
+        el.click();
+      } else if (el === 'setIntervalPlaceholder' && !window.githubBookmarkletLoadMoreInterval) {
+        window.githubBookmarkletLoadMoreInterval = setInterval(setButtonClickTimeouts, 1000);
+      }
+    }, index * 300);
+  });
+}
+
+setButtonClickTimeouts();

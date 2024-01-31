52a90b9a615da4428fa79d9345309209628b06e0
David Runger <david.runger@commonlit.org>
2024-01-31 15:27:39 -0600

    [bookmarklets/sentry/scrape-column] Add bookmarklet to scrape column

---
+++ tools/bookmarklets/sentry/scrape-column.js
@@ -0,0 +1,29 @@
+// Paste this into e.g. https://caiorss.github.io/bookmarklet-maker/ to convert
+// this to bookmarklet code.
+
+window.answer = [];
+const columnIndex = parseInt(prompt('Zero-indexed column number?'), 10);
+
+function scrapeColumn() {
+  document
+    .querySelectorAll('[data-test-id=grid-body-cell]')
+    .forEach((element, index) => {
+      if (index % 12 === columnIndex) answer.push(element.innerText);
+    });
+
+  console.log(answer);
+
+  const nextButton = document.querySelector(
+    '[data-source-file="pagination.tsx"][aria-label="Next"]:not([disabled])'
+  );
+
+  if (nextButton) {
+    nextButton.click();
+  } else {
+    clearInterval(interval);
+    console.log('Done! Run `copy(window.answer)`.');
+  }
+}
+
+const interval = setInterval(scrapeColumn, 5000);
+scrapeColumn();

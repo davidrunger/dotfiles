be6240b0e5515ab50b886123b2405ef8ff820bfb
David Runger <david.runger@commonlit.org>
2024-02-01 22:05:56 -0600

    [string_patches] Extract String monkey patches to a dedicated file
    This allows requiring it in other places (in addition to
    monkeypatch_repl, from which I am now extracting it).

--- utils/ruby/monkeypatch_repl.rb
+++ utils/ruby/monkeypatch_repl.rb
@@ -2,6 +2,7 @@
 
 # Make it easy to copy any object as a string to the clipboard.
 require_relative "#{Dir.home}/code/dotfiles/utils/ruby/copy_utils.rb"
+require_relative "#{Dir.home}/code/dotfiles/utils/ruby/string_patches.rb"
 
 # rubocop:disable Style/TopLevelMethodDefinition
 def skip_for!(seconds) ;
@@ -55,47 +56,6 @@ class Object
   # copy for a spec expectation
   def cpps
     ai.uncolor.cpp
-  end
-end
-
-class String
-  def cpp
-    # copying SQL, e.g. User.select(:id, :email).to_sql.cpp
-    if start_with?('SELECT')
-      super(tr('"', '')) # remove double quotes (from around table names, etc.)
-    else
-      super(self)
-    end
-  end
-
-  # "Sublime regex (for searching)"
-  def sr
-    underscore.gsub('_', '.?').cpp
-  end
-
-  def upp
-    underscore.cpp
-  end
-
-  def const
-    gsub(%r{((app/\w+)|lib)/}, "").
-      gsub(/\d{4}-\d{2}-\d{2}-/, "").
-      gsub(".rb", "").
-      underscore.
-      camelize.
-      cpp
-  end
-
-  def spec
-    gsub(%r{\A(lib/)}, "").
-      then { |path| "spec/#{path}" }.
-      gsub(%r{\.rb}, "_spec.rb").
-      cpp
-  end
-
-  # Remove colorizing ANSI escape codes.
-  def uncolor
-    gsub(/\e\[([;\d]+)?m/, "")
   end
 end
 
---
+++ utils/ruby/string_patches.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class String
+  def cpp
+    # copying SQL, e.g. User.select(:id, :email).to_sql.cpp
+    if start_with?('SELECT')
+      super(tr('"', '')) # remove double quotes (from around table names, etc.)
+    else
+      super(self)
+    end
+  end
+
+  # "Sublime regex (for searching)"
+  def sr
+    underscore.gsub('_', '.?').cpp
+  end
+
+  def upp
+    underscore.cpp
+  end
+
+  def const
+    gsub(%r{((app/\w+)|lib)/}, '').
+      gsub(/\d{4}-\d{2}-\d{2}-/, '').
+      gsub('.rb', '').
+      underscore.
+      camelize.
+      cpp
+  end
+
+  def spec
+    gsub(%r{\A(lib/)}, '').
+      then { |path| "spec/#{path}" }.
+      gsub(%r{\.rb}, '_spec.rb').
+      cpp
+  end
+
+  # Remove colorizing ANSI escape codes.
+  def uncolor
+    gsub(/\e\[([;\d]+)?m/, '')
+  end
+end

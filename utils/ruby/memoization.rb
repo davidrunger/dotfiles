dc9aedd1a61e1ba34b88cfee738dba54b56685a8
David Runger <davidjrunger@gmail.com>
2024-02-24 22:49:52 -0600

    [memoization] Add wrapper around memo_wise with less awkward name

---
+++ utils/ruby/memoization.rb
@@ -0,0 +1,16 @@
+if !defined?(MemoWise)
+  require_relative "#{Dir.home}/code/dotfiles/utils/ruby/load_gem.rb"
+  load_gem 'memo_wise'
+end
+
+module MemoWisePatches
+  def prepended(target)
+    super
+
+    target.singleton_class.alias_method(:memoize, :memo_wise)
+  end
+end
+
+MemoWise.singleton_class.prepend(MemoWisePatches)
+
+Memoization = MemoWise

4537099ebf5e7d5b267d995ceade91af52aa499e
David Runger <david.runger@commonlit.org>
2024-03-05 22:17:38 -0600

    [hash_patches] Add #digc to dig with case equality
    This is currently not used in any dotfiles, but it is used in my
    `z-dr-response-modifier-middleware` branch.

---
+++ utils/ruby/hash_patches.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class Hash
+  # [dig] with [c]ase equality
+  #
+  # Example:
+  # hash = {
+  #   "GET" =>
+  #     {
+  #       "/user/auth/google_oauth2/callback?" => {
+  #         code: 499,
+  #         headers: {},
+  #         body_string: "",
+  #       },
+  #     },
+  # }
+  # hash.digc("GET", %r{/user/auth/google_oauth2/callback\?}).tapp
+  def digc(*keys)
+    current_value = self
+
+    keys.tapp.each do |key|
+      value_at_current_key =
+        current_value.tapp.
+          detect do |inner_key, _value|
+            inner_key.tapp ===
+              key.tapp
+          end&.second
+
+      if value_at_current_key.tapp.nil?
+        return nil
+      elsif value_at_current_key.is_a?(Hash)
+        current_value = value_at_current_key
+      end
+    end
+
+    current_value
+  end
+end

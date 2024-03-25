66af3fdc693ecba7144a83208950deb629251e35
David Runger <davidjrunger@gmail.com>
2024-03-25 12:49:48 -0500

    [linux][Postgres@16] Add Postgres 16 to PATH

--- shell/linux.sh
+++ shell/linux.sh
@@ -1,1 +1,6 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# Postgres 16
+if [ -d "/home/linuxbrew/.linuxbrew/opt/postgresql@16/bin" ]; then
+  export PATH="/home/linuxbrew/.linuxbrew/opt/postgresql@16/bin:$PATH"
+fi

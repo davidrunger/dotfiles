9e039f058ec1ac8cff694401a7f7d57734fb5432
David Runger <davidjrunger@gmail.com>
2024-03-29 20:10:37 -0500

    [linux] Remove duplicate addition of Postgres@16 to PATH

--- shell/linux.sh
+++ shell/linux.sh
@@ -1,6 +1,1 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# Postgres 16
-if [ -d "/home/linuxbrew/.linuxbrew/opt/postgresql@16/bin" ]; then
-  export PATH="/home/linuxbrew/.linuxbrew/opt/postgresql@16/bin:$PATH"
-fi

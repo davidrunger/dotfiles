d25341d41eaeafb0f0315ded12211d9500ea27d1
David Runger <davidjrunger@gmail.com>
2024-06-16 21:27:05 -0500

    [update-gems] Add script to update gems in all repos w/ Gemfile.lock

---
+++ tools/update-gems.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Updates the gems (per Gemfile.lock) for all repositories with updateable gems.
+
+# Tip: after running this and merging the PRs, then run
+# ./tools/delete-merged-branches.sh to delete the branches.
+
+set -uo pipefail # don't allow undefined variables, pipes don't swallow errors
+
+cd "$HOME/code" || exit
+
+for dir in */ ; do
+  cd "$dir" || exit
+  blue "# $dir"
+
+  if test -e Gemfile.lock ; then
+    set -ex
+
+    if ! branch-exists 'bundle-update' ; then
+      bundle update
+
+      if ! git diff --quiet ; then
+        gstash
+        gfcob bundle-update
+        gunstash
+        gacm 'Update gems
+
+`bundle update`'
+        hpr
+      fi
+    fi
+
+    { set +ex; } 2>/dev/null
+  fi
+
+  echo
+
+  cd - > /dev/null 2>&1 || exit
+done

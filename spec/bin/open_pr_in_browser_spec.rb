55e38cd5e96f25002d8cfca38acabd945a1abd46
David Runger <davidjrunger@gmail.com>
2024-06-25 19:00:56 -0500

    [open_pr_in_browser_spec] Change commonlit example to a generic one

--- spec/bin/open_pr_in_browser_spec.rb
+++ spec/bin/open_pr_in_browser_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Run these tests with:
-#     gal spec/bin/open-pr-in-browser_spec.rb
+#     gal spec/bin/open_pr_in_browser_spec.rb
 
 load File.expand_path('../../bin/open-pr-in-browser', __dir__)
 
@@ -11,13 +11,13 @@ RSpec.describe(OpenPrInBrowser) do
   describe '#pr_link' do
     subject(:pr_link) { runner.pr_link }
 
-    context 'when the output includes a PR link for commonlit' do
+    context 'when the output includes a PR link for a workplace' do
       let(:create_pr_command_output) do
-        'content https://github.com/commonlit/commonlit/pull/198 more content'
+        'content https://github.com/software-company/the-app/pull/198 more content'
       end
 
       it 'returns the PR link' do
-        expect(pr_link).to eq('https://github.com/commonlit/commonlit/pull/198')
+        expect(pr_link).to eq('https://github.com/software-company/the-app/pull/198')
       end
     end
 

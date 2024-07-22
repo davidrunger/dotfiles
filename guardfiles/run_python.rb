f6b4a7876d12c720ec19f56b9cd7ccc305a777ea
David Runger <davidjrunger@gmail.com>
2024-07-22 10:38:53 -0500

    [gal] Add a Python runner [DOT-51] (#96)
    I want to use this to aid in kitty-opener debugging (since that uses
    Python).

---
+++ guardfiles/run_python.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# This is used by `gal` when the `python` `--guardfile` option is used.
+
+require_relative "#{Dir.home}/code/dotfiles/utils/ruby/guard_shell_with_guard_monkeypatch.rb"
+
+guard(:shell, all_on_start: true) do
+  directories_to_watch = %w[app bin lib personal spec].select { Dir.exist?(_1) }
+
+  # https://web.archive.org/web/20200927034139/https://github.com/guard/listen/wiki/Duplicate-directory-errors
+  directories(directories_to_watch)
+
+  watch_regex =
+    %r{^(
+      #{directories_to_watch.map { "#{_1}/.*.py" }.join("|\n")}
+    )}x
+
+  watch(watch_regex) do |guard_match_result|
+    begin
+      match = guard_match_result.instance_variable_get(:@match_result) || '[no match]'
+      puts("Match for #{match} triggered execution.")
+      system('clear', exception: true)
+      system('python3 personal/python.py', exception: true)
+    rescue => error
+      pp(error)
+    end
+    # rubocop:disable Rails/TimeZone, Lint/RedundantCopDisableDirective
+    puts("Ran at #{Time.new}")
+    # rubocop:enable Rails/TimeZone, Lint/RedundantCopDisableDirective
+  end
+end

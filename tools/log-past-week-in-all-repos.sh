99c84781114f261c9168f771c6ec1e68c54707c3
David Runger <davidjrunger@gmail.com>
2024-07-25 22:57:36 -0500

    Add script to show git logs over past X days in all repos [DOT-59] (#116)
    I'm not sure how often I'll actually use this, but let's commit it for
    now and see if it's useful enough / used enough to keep. I think we
    should consider deleting this if I end up using it only very rarely or
    never.

---
+++ tools/log-past-week-in-all-repos.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # exit on any error, don't allow undefined variables, pipes don't swallow errors
+
+cd "$HOME/code" || exit
+
+for dir in $(my-repos) ; do
+  cd "$dir" || exit
+  blue "# $dir"
+
+  sha_to_diff_against=$(git rev-list "$(main-branch)" --after="7 days ago" --reverse HEAD | sed -n '1p')
+
+  if [ "$sha_to_diff_against" = "" ] ; then
+    red 'No recent commits.'
+  else
+    git --no-pager log --oneline "$sha_to_diff_against^.." --color=always | rg -v '^.{0,12}[0-9a-f]{5,40}.{0,12} Bump '
+  fi
+
+  echo
+
+  cd - &>/dev/null || exit
+done

276d1f2d64606894f444b68e6f454ba5021fc9f7
David Runger <davidjrunger@gmail.com>
2024-08-07 17:36:20 -0500

    [wait-for-gh-checks] Source the number of checks via runger-config (#152)
    ... rather than via a YAML file in dotfiles-personal.

---
+++ utils/ruby/runger_config.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# This is a Ruby binding to read the configuration managed by `runger-config`.
+
+require_relative 'memoization.rb'
+
+class RungerConfig
+  include Singleton
+  prepend Memoization
+
+  class << self
+    def [](key)
+      instance.unified_config[key]
+    end
+  end
+
+  memoize \
+  def unified_config
+    public_config.merge(private_config)
+  end
+
+  private
+
+  memoize \
+  def public_config
+    parsed_yaml('.runger-config.yml')
+  end
+
+  memoize \
+  def private_config
+    parsed_yaml('.runger-config.private.yml')
+  end
+
+  def parsed_yaml(file_name)
+    if File.exist?(file_name)
+      content = File.read(file_name)
+      YAML.load(content)
+    else
+      {}
+    end
+  end
+end

c710419e839fcdf25a977ffc9185dcd1ee6342c6
David Runger <davidjrunger@gmail.com>
2024-08-22 01:01:55 -0500

    [flag-unacked-file-versions] Convert to Crystal (from Ruby) [DOT-76] (#190)
    The `Memoization` macro added in this PR is borrowed with modification
    from these places:
    -
    https://github.com/luckyframework/lucky/blob/dd21f8bf9031ed444797534cab07fcbbb149e950/src/lucky/memoizable.cr
    (mostly this)
    -
    https://gist.github.com/straight-shoota/3ef2fe2f597a4306ef9b84d195f81bf3

---
+++ utils/crystal/runger_config.cr
@@ -0,0 +1,38 @@
+require "yaml"
+
+require "./memoization"
+
+class RungerConfig
+  def self.[](key)
+    instance.unified_config[key]
+  end
+
+  def self.has_key?(key)
+    instance.unified_config.has_key?(key)
+  end
+
+  def self.instance
+    @@instance ||= new
+  end
+
+  memoize def unified_config : Hash(String, YAML::Any)
+    public_config.merge(private_config)
+  end
+
+  memoize def public_config : Hash(String, YAML::Any)
+    parsed_yaml(".runger-config.yml")
+  end
+
+  memoize def private_config : Hash(String, YAML::Any)
+    parsed_yaml(".runger-config.private.yml")
+  end
+
+  private def parsed_yaml(file_name : String) : Hash(String, YAML::Any)
+    if File.exists?(file_name)
+      content = File.read(file_name)
+      YAML.parse(content).as_h.transform_keys { |key| key.to_s }
+    else
+      {} of String => YAML::Any
+    end
+  end
+end

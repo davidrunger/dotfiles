b759aa3ad2daf0e56ee20b1ef0bcf9e140bbf740
David Runger <davidjrunger@gmail.com>
2024-09-13 12:10:09 -0500

    [zshrc] Add fzf-tab plugin
    I'm going to at least try this for a while. I am guessing that I will
    like it and stick with it, but we'll see.

--- zshrc.zsh
+++ zshrc.zsh
@@ -20,6 +20,7 @@ setopt +o nomatch # https://unix.stackexchange.com/a/310553/276727
 export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="bolso"
 plugins=(
+  fzf-tab # git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
   zsh-autosuggestions # git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   zsh-syntax-highlighting # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 )

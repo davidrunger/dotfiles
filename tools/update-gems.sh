60e5ad7552a18f3d5befc51f2f1d5cad83d7031b
David Runger <davidjrunger@gmail.com>
2025-01-23 01:20:31 -0600

    [update-gems] Exit with 1 on cd failure
    I don't really know how we could reach this code, but, if we somehow do,
    then it seems to me that we should exit with failure (1) rather than 0
    (success).

--- tools/update-gems.sh
+++ tools/update-gems.sh
@@ -7,13 +7,13 @@
 
 set -uo pipefail # don't allow undefined variables, pipes don't swallow errors
 
-cd "$HOME/code" || exit
+cd "$HOME/code" || exit 1
 
 branch_name="bundle-update"
 ignore_dirs=$(runger-config --show forks | paste -sd '|' -)
 
 for dir in $(my-repos) ; do
-  cd "$dir" || exit
+  cd "$dir" || exit 1
   blue "# $dir"
 
   if [ -f Gemfile.lock ] && ! [[ "$dir" =~ ^(${ignore_dirs})$ ]] ; then
@@ -39,5 +39,5 @@ for dir in $(my-repos) ; do
 
   echo
 
-  cd - &>/dev/null || exit
+  cd - &>/dev/null || exit 1
 done

55080e7eec33c1d0574168ae8a20673de760e318
David Runger <davidjrunger@gmail.com>
2025-01-29 15:31:31 -0600

    [ga] Move from alias to script

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -12,7 +12,6 @@ alias down='cd ~/Downloads'
 alias fix='git diff --name-only | uniq | xargs $EDITOR'
 alias fs='format-sql'
 alias fsk='redis-cli -n 1 FLUSHDB && sk' # `-n 1` because of `REDIS_DATABASE_NUMBER=1` in `.env`
-alias ga='git add'
 alias gac='ga . && gcom'
 alias gba='GIT_PAGER=cat git branch -vv'
 alias gbdf='git branch -D $(active-branches | fzf)'

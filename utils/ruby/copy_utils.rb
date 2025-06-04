8e23dde1c6ee3c3eabb4c64ec54cbafdfaee22c9
David Runger <davidjrunger@gmail.com>
2025-06-04 09:31:19 -0500

    Fix RuboCop complaint(s)

--- utils/ruby/copy_utils.rb
+++ utils/ruby/copy_utils.rb
@@ -8,13 +8,14 @@ module CopyUtils
   # Copies the object upon which this method is called as a string into the clipboard.
   def cpp(input = nil)
     str = (input || self).to_s
+
     Clipboard.copy(str)
+
     if str.size < 100
       puts(AmazingPrint::Colors.green("Copied '#{str}' to clipboard."))
     else
       puts(AmazingPrint::Colors.green("Copied #{str.size} characters to clipboard."))
     end
-    true
   end
 end
 

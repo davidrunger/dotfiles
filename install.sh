1304f764158fb04ecf2c7ded15e77e039408b904
David Runger <davidjrunger@gmail.com>
2026-09-14 23:53:27 -0500

    [install] Add basher setup script
    https://claude.ai/share/41815633-bd72-4d0c-9250-0539eaa21677

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ if [ "${FULL_INSTALL:-0}" = "1" ]; then
 
   # NOTE: Some of these depend on earlier steps, like `brew bundle`.
   ~/code/dotfiles/install/apt-packages.sh
+  ~/code/dotfiles/install/basher.sh
   ~/code/dotfiles/install/mitmproxy-env.sh
   ~/code/dotfiles/install/oh-my-zsh.sh
   ~/code/dotfiles/install/ufw.sh
